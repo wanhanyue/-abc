fb1bb4ca80226ba8c56c8ed189b194c3ff9f3ec2
万寒月 <1912143442@qq.com>
2018-05-11 00:55:30 +0800

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+
 const fibonacci = require('./lib/fibonacci')
+
 const anagram = require('./lib/anagram')
 
+
 console.log(anagram("abcd"))
+const 
+
+isLeapYear= require('./lib/
+
+isLeapYear')
 
